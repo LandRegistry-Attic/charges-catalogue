b2397633791ea990d46d72b5064ea9f280582751
Michael Allen <michael@michaelallen.io>
2015-04-23 15:28:28 +0100

    create a sass filter for flaskassets

--- app/static/pipeline.py
+++ app/static/pipeline.py
@@ -1,5 +1,10 @@
 from flask.ext.assets import Bundle
+import sass as libsass
+
+
+def __compile_sass(_in, out, **kw):
+    out.write(libsass.compile(string=_in.read()))
 
 
 sass = Bundle('sass/main.scss',
-              output='gen/main.css')
+              filters=(__compile_sass,), output='gen/main.css')
