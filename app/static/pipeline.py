570185df21d632e693a1df6fb073354b2c5193f8
Michael Allen <michael@michaelallen.io>
2015-04-24 14:37:33 +0100

    wip: working sass compilation that imports files from the govuk_frontend_toolkit

--- app/static/pipeline.py
+++ app/static/pipeline.py
@@ -1,9 +1,19 @@
 from flask.ext.assets import Bundle
+from os import path
 import sass as libsass
 
 
+__dot = path.dirname(path.realpath(__file__))
+__toolkit_dir = path.join(__dot, 'govuk_frontend_toolkit/stylesheets/')
+
+
 def __compile_sass(_in, out, **kw):
-    out.write(libsass.compile(string=_in.read()))
+    out.write(
+        libsass.compile(
+            string=_in.read(),
+            include_paths=[__toolkit_dir]
+        )
+    )
 
 
 sass = Bundle('sass/main.scss',
