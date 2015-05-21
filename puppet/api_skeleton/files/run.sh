a5d7337afda5f1baab431b7d8e6804cdb29701e4
phemus <ramin.vazir@landregistry.gsi.gov.uk>
2015-05-21 14:49:10 +0100

    added run script to puppet

---
+++ puppet/api_skeleton/files/run.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+virtualenv ./.service_env
+
+source ./.service_env/bin/activate
+
+pip3 install -r requirements.txt
+
+port="${API_SKELETON_GUNICORN_PORT:-8000}"
+host="${API_SKELETON_GUNICORN_HOST:-0.0.0.0}"
+
+
+gunicorn -b $host:$port --pid /var/run/api_skeleton.pid "app:create_manager().app"
--- puppet/api_skeleton/manifests/init.pp
+++ puppet/api_skeleton/manifests/init.pp
@@ -1,6 +1,6 @@
 # Install and configure the Flask Api Skeleton
 class api_skeleton (
-    $port = '9000',
+    $port = '9010',
     $host = '0.0.0.0',
     $branch_or_revision = 'master'
 ) {
@@ -13,6 +13,36 @@ class api_skeleton (
     revision => $branch_or_revision,
     owner    => 'vagrant',
     group    => 'vagrant',
+    notify  => Service['api_skeleton'],
+  }
+
+  file { '/opt/api-skeleton/bin/run.sh':
+    ensure  => 'file',
+    mode    => '0755',
+    owner   => 'vagrant',
+    group   => 'vagrant',
+    source  => "puppet:///modules/${module_name}/run.sh",
+    require => Vcsrepo['/opt/api-skeleton'],
+    notify  => Service['api_skeleton'],
+  }
+
+  file { '/etc/systemd/system/api_skeleton.service':
+    ensure  => 'file',
+    mode    => '0755',
+    owner   => 'vagrant',
+    group   => 'vagrant',
+    content => template("${module_name}/service.systemd.erb"),
+    notify  => [Exec['systemctl-daemon-reload'], Service['api_skeleton']],
+  }
+  service { 'api_skeleton':
+    ensure   => 'running',
+    enable   => true,
+    provider => 'systemd',
+    require  => [
+      Vcsrepo['/opt/api-skeleton'],
+      File['/opt/api-skeleton/bin/run.sh'],
+      File['/etc/systemd/system/api_skeleton.service']
+    ],
   }
 
 }
